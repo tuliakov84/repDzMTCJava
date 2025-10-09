5e4482d4f48d40eb43fbe742c41adf989a505145
Evgeniy <zz21marcel@gmail.com>
2025-10-10 00:24:39 +0300

    created files for hw4

---
+++ hm4/ArrayUtils.java
@@ -0,0 +1,33 @@
+public class ArrayUtils {
+  public static <T> int findFirst(T[] array, T element) {
+    if (array == null) {
+      return -1;
+    }
+
+    for (int i = 0; i < array.length; i++) {
+      if (element == null) {
+        if (array[i] == null) {
+          return i;
+        }
+      } else if (element.equals(array[i])) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    // пример использования
+    final String[] names = {"Alice", "Bob", "Charlie"};
+    final String[] lstTest = {"Alice", "Bob", "Charlie", null};
+    final int index = ArrayUtils.findFirst(names, "Bob"); // Ожидаем: 1 (тк нумерация в массиве начинается с нуля)
+    System.out.println(index);
+    System.out.println(ArrayUtils.findFirst(names, null));
+    System.out.println(ArrayUtils.findFirst(null, "Bob"));
+    System.out.println(ArrayUtils.findFirst(lstTest, null));
+    System.out.println(ArrayUtils.findFirst(new String[]{}, "Bob"));
+    System.out.println(ArrayUtils.findFirst(new String[]{}, null));
+    System.out.println(ArrayUtils.findFirst(names, "Bobis"));
+  }
+}
