6fa6a16362508bc53ff91e41920dcebeb1d39e83
Evgeniy <zz21marcel@gmail.com>
2025-11-23 13:05:07 +0300

    created Bank

---
+++ src/main/java/com/mipt/hw9_multithreading/Bank.java
@@ -0,0 +1,38 @@
+package com.mipt.hw9_multithreading;
+
+package com.mipt.hw9_multithreading;
+
+public class Bank {
+  public void sendToAccount(BankAccount source, BankAccount destination, int amount) {
+    if (source == null || destination == null) throw new IllegalArgumentException();
+
+    BankAccount firstAccount;
+    BankAccount secondAccount;
+
+    if (source.getId().compareTo(destination.getId()) < 0) {
+      firstAccount = source;
+      secondAccount = destination;
+    } else {
+      firstAccount = destination;
+      secondAccount = source;
+    }
+
+    synchronized (firstAccount) {
+      synchronized (secondAccount) {
+        source.withDraw(amount);
+        destination.deposit(amount);
+      }
+    }
+  }
+
+  public void sendToAccountDeadlock(BankAccount source, BankAccount destination, int amount) {
+    if (source == null || destination == null) throw new IllegalArgumentException();
+
+    synchronized (source) {
+      synchronized (destination) {
+        source.withDraw(amount);
+        destination.deposit(amount);
+      }
+    }
+  }
+}
