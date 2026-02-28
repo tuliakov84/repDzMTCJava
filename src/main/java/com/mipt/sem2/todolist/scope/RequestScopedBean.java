e7f945f4cba6af5c4ea3f3aa8f536c0e3c508a52
Evgeniy <zz21marcel@gmail.com>
2026-02-28 16:59:24 +0300

    создал тесты

---
+++ src/main/java/com/mipt/sem2/todolist/scope/RequestScopedBean.java
@@ -0,0 +1,26 @@
+package com.mipt.sem2.todolist.scope;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class RequestScopedBean {
+  private final String requestId = UUID.randomUUID().toString();
+  private final LocalDateTime startTime = LocalDateTime.now();
+
+  public String getRequestId() {
+    return requestId;
+  }
+
+  public LocalDateTime getStartTime() {
+    return startTime;
+  }
+
+  public long getProcessingTime() {
+    return java.time.Duration.between(startTime, LocalDateTime.now()).toMillis();
+  }
+}
